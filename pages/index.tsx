2cfc8373a35cecd09593d926294f9faafeac72d1
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-21 12:02:37 +0900

    * Update index looks

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,14 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
+      <Head>
+        <title>Merge</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <div className="text-center lg:text-left lg:pl-2">
+          <h1 className="text-5xl font-bold">Merge</h1>
+          <p className="py-6">Webapp to concentrate with your community or yourself.<br />Chat, Calender, Task(Todo), Vote, daialy and so on!</p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
@@ -29,6 +33,9 @@ const Home: NextPage = () => {
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary">Login</button>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary-content">or Signup</button>
             </div>
           </div>
         </div>
