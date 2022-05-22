fb43c5f70d841f02f67956017a91f3abe5dc6f69
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-22 11:05:25 +0900

    string and !

--- utils/supabaseClient.tsx
+++ utils/supabaseClient.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase;
