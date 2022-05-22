9c61f244641e4d0e47c61fec7a5e0c96c97405be
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-22 10:46:06 +0900

    Add more OAuth

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,9 +1,23 @@
-import { AuthLogin, signInWithDiscord } from "../lib/Auth";
-import supabase from "../utils/supabaseClient";
+import { AuthLogin, signInWithDiscord, signInWithSlack } from "../lib/Auth";
 import Link from "next/link";
-import { FaDiscord } from "react-icons/fa";
+import {
+  FaCircle,
+  FaDiscord,
+  FaFacebook,
+  FaSlack,
+  FaTwitter
+} from "react-icons/fa";
+import { useState } from "react";
 
 export function LoginComponent() {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const submitOnEnter = (event: { keyCode: number }) => {
+    // Watch for enter key
+    if (event.keyCode === 13) {
+      AuthLogin(email, password);
+    }
+  };
   return (
     <>
       <div className="form-control">
@@ -14,6 +28,8 @@ export function LoginComponent() {
           type="text"
           placeholder="email"
           className="input input-bordered"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <div className="form-control">
@@ -24,6 +40,9 @@ export function LoginComponent() {
           type="text"
           placeholder="password"
           className="input input-bordered"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          onKeyDown={(e) => submitOnEnter(e)}
         />
         <label className="label">
           <a href="#" className="label-text-alt link link-hover">
@@ -31,8 +50,11 @@ export function LoginComponent() {
           </a>
         </label>
       </div>
-      <div className="form-control mt-6">
-        <button className="btn btn-primary" onClick={() => AuthLogin()}>
+      <div className="form-control mt-3">
+        <button
+          className="btn btn-primary"
+          onClick={() => AuthLogin(email, password)}
+        >
           Login
         </button>
         <div className="py-1" />
@@ -45,15 +67,53 @@ export function LoginComponent() {
           <div className="collapse-title text-xl font-medium">
             Or use third-party OAuth!
           </div>
-          <div className="collapse-content">
+          <div className="collapse-content grid gap-y-1">
             <button
               className="btn btn-primary-content w-full"
               onClick={() => signInWithDiscord()}
             >
               <FaDiscord />
-              <span className="pl-1" />
+              <span className="pl-2" />
               Discord
             </button>
+            <button
+              className="btn btn-primary-content w-full"
+              onClick={() => signInWithSlack()}
+            >
+              <FaSlack />
+              <span className="pl-2" />
+              Slack
+            </button>
+            <div
+              className="tooltip tooltip-top tooltip-warning"
+              data-tip="Sorry, we are not supporting Twitter now"
+            >
+              <button className="btn w-full" disabled={true}>
+                <FaTwitter />
+                <span className="pl-2" />
+                Twitter
+              </button>
+            </div>
+            <div
+              className="tooltip tooltip-top tooltip-warning"
+              data-tip="Sorry, we are not supporting Facebook now"
+            >
+              <button className="btn w-full" disabled={true}>
+                <FaFacebook />
+                <span className="pl-2" />
+                Facebook
+              </button>
+            </div>
+            <div
+              className="tooltip tooltip-top tooltip-warning"
+              data-tip="Sorry, we are not supporting Zoom now"
+            >
+              <button className="btn w-full" disabled={true}>
+                <FaCircle />
+                <span className="pl-2" />
+                Zoom
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- lib/Auth.tsx
+++ lib/Auth.tsx
@@ -1,14 +1,27 @@
 import supabase from "../utils/supabaseClient";
 export async function AuthLogin(email: any, password: any) {
-  const { user, session, error } = await supabase.auth.signIn({
-    email: email,
-    password: password
-  });
-  return null;
+  try {
+    const { error, user } = await supabase.auth.signIn({ email, password });
+    // If the user doesn't exist here and an error hasn't been raised yet,
+    // that must mean that a confirmation email has been sent.
+    // NOTE: Confirming your email address is required by default.
+    if (error) {
+      alert("Error with auth: " + error.message);
+    }
+  } catch (error) {
+    console.log("error", error);
+    alert(error.error_description || error);
+  }
 }
 
 export async function signInWithDiscord() {
   const { user, session, error } = await supabase.auth.signIn({
     provider: "discord"
+  });
+}
+
+export async function signInWithSlack() {
+  const { user, session, error } = await supabase.auth.signIn({
+    provider: "slack"
   });
 }
